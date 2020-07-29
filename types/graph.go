9503dffd68e1b5927d14857d7dd9c39753395784
jess <jesse@soob.co>
2020-07-29 20:25:23 +0900

    fix: interface acceptance for dependencies

--- types/graph.go
+++ types/graph.go
@@ -14,5 +14,9 @@ type BaseFilter struct {
 
 type GraphQLQueryRaw interface{}
 type GraphQLParams map[string]interface{}
-type GraphQLQuerier func(query string, variables GraphQLParams) *graphql.Result
+type GraphQLQuerier func(
+	query string,
+	variables GraphQLParams,
+	dependencies []ModelType,
+) *graphql.Result
 type GraphQLCommitter func(entity interface{})
