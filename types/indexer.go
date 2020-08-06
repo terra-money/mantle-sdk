c6f091fe5c7d0e204404ee8dee7f36600e1cd91f
jess <jesse@soob.co>
2020-08-07 08:46:40 +0900

    fix: indexer function must return error

--- types/indexer.go
+++ types/indexer.go
@@ -2,7 +2,7 @@ package types
 
 type IndexerQuerier func(request interface{}, variables GraphQLParams) error
 type IndexerCommitter func(entity interface{}) error
-type Indexer func(query Query, commit Commit)
+type Indexer func(query Query, commit Commit) error
 type IndexerRegisterer func(Register)
 
 // some aliases
