21cd84534098e2fecffecac3e9bed77c4b5f7bd2
jess <jesse@soob.co>
2020-08-26 18:05:18 +0900

    refactor(schemabuilders/models): allow listgen

--- querier/queryhandler/height.go
+++ querier/queryhandler/height.go
@@ -1,70 +1,124 @@
 package queryhandler
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/terra-project/mantle/db"
-	"github.com/terra-project/mantle/db/kvindex"
 	"github.com/terra-project/mantle/utils"
 )
 
 type HeightResolver struct {
-	db           db.DB
-	entityName   string
-	indexName    string
-	seekKey      []byte
+	db          db.DB
+	entityName  string
+	indexName   string
+	prefixStart []byte
+	prefixEnd   []byte
 }
 
 // seek resolver
 func NewHeightResolver(
 	db db.DB,
-	kvIndex *kvindex.KVIndex,
+	_,
 	entityName,
 	indexName string,
 	indexOption interface{},
 ) (QueryHandler, error) {
-	if indexName != "Height" {
+	if !strings.HasPrefix(indexName, "Height") {
 		return nil, nil
 	}
 
-	heightInUint64, _ := utils.GetUint64FromWhatever(indexOption)
-	seekKey := utils.ConcatBytes([]byte(entityName), utils.LeToBe(heightInUint64))
+	if heightRange, isHeightRange := indexOption.([]uint64); isHeightRange {
+		return &HeightResolver{
+			db:          db,
+			entityName:  entityName,
+			indexName:   indexName,
+			prefixStart: utils.LeToBe(heightRange[0]),
+			prefixEnd:   utils.LeToBe(heightRange[1]),
+		}, nil
+	}
 
-	return &HeightResolver{
-		db:           db,
-		entityName:   entityName,
-		indexName:    indexName,
-		seekKey:      seekKey,
-	}, nil
+	if heightSingle, isHeightSingle := indexOption.(uint64); isHeightSingle {
+		return &HeightResolver{
+			db:          db,
+			entityName:  entityName,
+			indexName:   indexName,
+			prefixStart: utils.LeToBe(heightSingle),
+			prefixEnd:   nil,
+		}, nil
+	}
+
+	return nil, fmt.Errorf("invalid height parameters, entityName=%s, indexOption=%v", entityName, indexOption)
 }
 
 func (resolver HeightResolver) Resolve() (QueryHandlerIterator, error) {
-	return NewHeightResolverIterator(resolver.seekKey), nil
+	entityNameInBytes := []byte(resolver.entityName)
+	indexNameInBytes := []byte(resolver.indexName)
+
+	prefixGroup := utils.BuildIndexGroupPrefix(
+		entityNameInBytes,
+		indexNameInBytes,
+	)
+
+	prefixStart := utils.BuildIndexIteratorPrefix(
+		entityNameInBytes,
+		indexNameInBytes,
+		resolver.prefixStart,
+	)
+
+	var prefixEnd []byte = nil
+	if resolver.prefixEnd != nil {
+		prefixEnd = utils.BuildIndexIteratorPrefix(
+			entityNameInBytes,
+			indexNameInBytes,
+			resolver.prefixEnd,
+		)
+	}
+
+	return NewHeightResolverIterator(
+		entityNameInBytes,
+		prefixGroup,
+		prefixStart,
+		prefixEnd,
+		resolver.db.IndexIterator(
+			prefixStart,
+			true,
+		),
+	), nil
 }
 
-// SeekResolverIterator never really iterates.
-// Implemented this way because of interface acceptance.
-// All methods (Valid, Next, Key, Close) will work to resolve documentKey
-// only one time.
-type HeightResolverIterator struct{
-	key []byte
-	isResolved bool
+type HeightResolverIterator struct {
+	entityName  []byte
+	prefixGroup []byte
+	prefixStart []byte
+	prefixEnd   []byte
+	it          db.Iterator
+	isResolved  bool
 }
 
-func NewHeightResolverIterator(key []byte) QueryHandlerIterator {
+func NewHeightResolverIterator(entityName, prefixGroup, prefixStart, prefixEnd []byte, it db.Iterator) QueryHandlerIterator {
 	return &HeightResolverIterator{
-		key: key,
-		isResolved: false,
+		entityName:  entityName,
+		prefixGroup: prefixGroup,
+		prefixStart: prefixStart,
+		prefixEnd:   prefixEnd,
+		it:          it,
 	}
 }
 
 func (resolver *HeightResolverIterator) Valid() bool {
-	return !resolver.isResolved
+	// isPrefixValid := resolver.it.Valid(resolver.prefixGroup)
+	return resolver.it.Valid(resolver.prefixGroup)
 }
 func (resolver *HeightResolverIterator) Next() {
-	resolver.isResolved = true
+	resolver.it.Next()
 }
 func (resolver *HeightResolverIterator) Key() []byte {
-	return resolver.key
+	return utils.BuildDocumentKey(
+		resolver.entityName,
+		resolver.it.DocumentKey(),
+	)
 }
 func (resolver *HeightResolverIterator) Close() {
-	// noop
+	resolver.it.Close()
 }
