dcb0c4534a8041db1afcbbb2f047172f6f9212c9
Jesse Chung <jesse@soob.co>
2020-09-23 18:23:59 +0900

    fix(graph/local_run): argument mapping

--- graph/local_run.go
+++ graph/local_run.go
@@ -39,9 +39,26 @@ func InternalGQLRun(p graphql.Params) *types.GraphQLInternalResult {
 			fieldName := selection.Name
 			fieldConfig := fields[fieldName.Value]
 
+			var variables = make(map[string]interface{})
+
+			// map arguments to query arguments
+			if len(selection.Arguments) > 0 {
+				for _, argument := range selection.Arguments {
+					argumentName := argument.Name.Value
+					switch argumentValue := argument.Value.GetValue().(type) {
+					case ast.Variable:
+						variables[argumentName] = p.VariableValues[argumentValue.Name.Value]
+					default:
+						variables[argumentName] = argumentValue
+					}
+				}
+			}
+
+			//args := getArgumentValues(fieldDef.Args, fieldAST.Arguments, eCtx.VariableValues)
+
 			rp := graphql.ResolveParams{
 				Source:  source,
-				Args:    p.VariableValues,
+				Args:    variables,
 				Info:    graphql.ResolveInfo{},
 				Context: p.Context,
 			}
@@ -76,3 +93,35 @@ func InternalResultInvariant(errors []error) *types.GraphQLInternalResult {
 		Errors: errors,
 	}
 }
+
+//
+//// Prepares an object map of argument values given a list of argument
+//// definitions and list of argument AST nodes.
+//func getArgumentValues(
+//	argDefs []*graphql.Argument, argASTs []*ast.Argument,
+//	variableValues map[string]interface{}) map[string]interface{} {
+//
+//	argASTMap := map[string]*ast.Argument{}
+//	for _, argAST := range argASTs {
+//		if argAST.Name != nil {
+//			argASTMap[argAST.Name.Value] = argAST
+//		}
+//	}
+//	results := map[string]interface{}{}
+//	for _, argDef := range argDefs {
+//		var (
+//			tmp   interface{}
+//			value ast.Value
+//		)
+//		if tmpValue, ok := argASTMap[argDef.PrivateName]; ok {
+//			value = tmpValue.Value
+//		}
+//		if tmp = valueFromAST(value, argDef.Type, variableValues); isNullish(tmp) {
+//			tmp = argDef.DefaultValue
+//		}
+//		if !isNullish(tmp) {
+//			results[argDef.PrivateName] = tmp
+//		}
+//	}
+//	return results
+//}
