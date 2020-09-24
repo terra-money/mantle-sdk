981b281ef31542be21ea3f6ba7903b3abd7fc8e3
Jesse Chung <jesse@soob.co>
2020-09-25 00:43:08 +0900

    refactor(graph/utils): use serdes

--- graph/utils.go
+++ graph/utils.go
@@ -1,36 +1,18 @@
 package graph
 
 import (
-	"encoding/json"
-	"github.com/graphql-go/graphql"
+	"github.com/terra-project/mantle/serdes"
 	"github.com/terra-project/mantle/types"
-	"github.com/vmihailenco/msgpack/v5"
 	"reflect"
 )
-
-// TODO: Make a better version of this or scrap
-func UnmarshalGraphQLResult(result *graphql.Result, target interface{}) error {
-	// leave BaseState alone
-	res, err := json.Marshal(result.Data)
-	if err != nil {
-		return err
-	}
-
-	err = json.Unmarshal(res, target)
-
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
 
 func UnmarshalInternalQueryResult(result *types.GraphQLInternalResult, target interface{}) error {
 	targetValue := reflect.Indirect(reflect.ValueOf(target))
 	for key, packBytes := range result.Data {
 		targetField := targetValue.FieldByName(key)
 		targetCache := reflect.New(targetField.Type())
-		if unpackErr := msgpack.Unmarshal(packBytes, targetCache.Interface()); unpackErr != nil {
+
+		if unpackErr := serdes.Deserialize(targetField.Type(), packBytes, targetCache.Interface()); unpackErr != nil {
 			return unpackErr
 		}
 
