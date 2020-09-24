cb60c7350f4f8685cb0d128d31782ded8a74f21e
Jesse Chung <jesse@soob.co>
2020-09-25 00:43:50 +0900

    feat(graph/generate/cosmos_scalars): add Buffer scalar type for []byte

--- graph/generate/cosmos_scalars.go
+++ graph/generate/cosmos_scalars.go
@@ -1,6 +1,8 @@
 package generate
 
 import (
+	"encoding/json"
+	"github.com/cosmos/cosmos-sdk/types"
 	"math/big"
 	"reflect"
 
@@ -8,14 +10,15 @@ import (
 	"github.com/graphql-go/graphql/language/ast"
 )
 
+var scalars = make(map[reflect.Type]ScalarGeneratorPair)
+
 // here defined are the list of known cosmos-sdk scalars.
 // when generating gql query or schema, we need to check if each
 // leaf value implements these interfaces,
 // and if so, the returned gql scalar type __must__ be used.
-
 func IsCosmosScalar(leafType reflect.Type) (*graphql.Scalar, bool) {
 	for _, test := range lists {
-		if reflect.PtrTo(leafType).Implements(test.Type) {
+		if test.Check(leafType, test.Scalar) {
 			return test.Scalar, true
 		}
 	}
@@ -24,18 +27,71 @@ func IsCosmosScalar(leafType reflect.Type) (*graphql.Scalar, bool) {
 }
 
 type ScalarGeneratorPair struct {
-	Type   reflect.Type
+	Check  func(target reflect.Type, scalar *graphql.Scalar) bool
 	Scalar *graphql.Scalar
 }
 
 var lists = []ScalarGeneratorPair{
+	// bigint
 	{
-		Type: reflect.TypeOf((*interface{ BigInt() *big.Int })(nil)).Elem(),
+		Check: func(target reflect.Type, scalar *graphql.Scalar) bool {
+			t := reflect.TypeOf((*interface{ BigInt() *big.Int })(nil)).Elem()
+			if target.Implements(t) {
+				return true
+			}
+			return false
+		},
 		Scalar: graphql.NewScalar(graphql.ScalarConfig{
 			Name:        "BigInt",
 			Description: "BigInt scalar type represents cosmos-sdk specific big int implementation",
 			Serialize: func(value interface{}) interface{} {
 				return value
+			},
+			ParseValue: func(value interface{}) interface{} {
+				return value
+			},
+			ParseLiteral: func(valueAST ast.Value) interface{} {
+				return valueAST.GetValue()
+			},
+		}),
+	},
+	{
+		Check: func(target reflect.Type, scalar *graphql.Scalar) bool {
+			t := reflect.TypeOf((*types.Msg)(nil)).Elem()
+			if target.Implements(t) {
+				return true
+			}
+			return false
+		},
+		Scalar: graphql.NewScalar(graphql.ScalarConfig{
+			Name:        "Msg",
+			Description: "Standard cosmos-sdk tx messages",
+			Serialize: func(value interface{}) interface{} {
+				msgInJsonBytes, _ := json.Marshal(value)
+				return string(msgInJsonBytes)
+			},
+			ParseValue: func(value interface{}) interface{} {
+				return value
+			},
+			ParseLiteral: func(valueAST ast.Value) interface{} {
+				return valueAST.GetValue()
+			},
+		}),
+	},
+
+	// byte buffer
+	{
+		Check: func(target reflect.Type, scalar *graphql.Scalar) bool {
+			if target == reflect.TypeOf(([]byte)(nil)) {
+				return true
+			}
+			return false
+		},
+		Scalar: graphql.NewScalar(graphql.ScalarConfig{
+			Name:        "Buffer",
+			Description: "[]byte serialized as string",
+			Serialize: func(value interface{}) interface{} {
+				return string(value.([]byte))
 			},
 			ParseValue: func(value interface{}) interface{} {
 				return value
