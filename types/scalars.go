ccf361d521708ff64ad08d372e8f21a248875d1c
Jesse Chung <jesse@soob.co>
2020-09-27 21:51:01 +0900

    feat(types/scalars): JSONScalar accepts custom marshaler

--- types/scalars.go
+++ types/scalars.go
@@ -5,12 +5,18 @@ import (
 )
 
 type JSONScalar string
+type JSONScalarCustomMarshaler func(originalData interface{}, data []byte) []byte
 
-func NewJSONScalar(data interface{}) JSONScalar {
+func NewJSONScalar(data interface{}, customMarshaller JSONScalarCustomMarshaler) JSONScalar {
 	bz, bzErr := json.Marshal(data)
 	if bzErr != nil {
 		panic(bzErr)
 	}
+
+	if customMarshaller != nil {
+		bz = customMarshaller(data, bz)
+	}
+
 	return JSONScalar(bz)
 }
 func (scalar *JSONScalar) IsJsonScalar() {}
