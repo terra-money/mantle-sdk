9de1e9008d1c1dd159558d83ef5d156b84e70466
Jesse Chung <jesse@soob.co>
2020-10-08 14:46:19 +0900

    fix(utils/responses): use full abci header when recreating block

--- utils/responses.go
+++ utils/responses.go
@@ -1,56 +1,36 @@
 package utils
 
 import (
-	"strconv"
-	"time"
-
 	abci "github.com/tendermint/tendermint/abci/types"
-	"github.com/terra-project/mantle/types"
+	tmtypes "github.com/tendermint/tendermint/types"
 )
 
-func ConvertToABCIHeader(header *types.Header) abci.Header {
-	var abciHeader = abci.Header{}
-
-	time, err := time.Parse(time.RFC3339, header.Time)
-
-	if err != nil {
-		panic(err)
+func ConvertToABCIHeader(header *tmtypes.Header) abci.Header {
+	var abciHeader = abci.Header{
+		Version: abci.Version{
+			Block: header.Version.Block.Uint64(),
+			App:   header.Version.App.Uint64(),
+		},
+		ChainID: header.ChainID,
+		Height:  header.Height,
+		Time:    header.Time,
+		LastBlockId: abci.BlockID{
+			Hash: header.LastBlockID.Hash,
+			PartsHeader: abci.PartSetHeader{
+				Total: int32(header.LastBlockID.PartsHeader.Total),
+				Hash:  header.LastBlockID.Hash,
+			},
+		},
+		LastCommitHash:     header.LastCommitHash,
+		DataHash:           header.DataHash,
+		ValidatorsHash:     header.ValidatorsHash,
+		NextValidatorsHash: header.NextValidatorsHash,
+		ConsensusHash:      header.ConsensusHash,
+		AppHash:            header.AppHash,
+		LastResultsHash:    header.LastResultsHash,
+		EvidenceHash:       header.EvidenceHash,
+		ProposerAddress:    header.ProposerAddress,
 	}
-
-	abciHeader.AppHash = []byte(header.AppHash)
-	abciHeader.ChainID = header.ChainID
-	abciHeader.Height = header.Height
-	abciHeader.Time = time
 
 	return abciHeader
-}
-
-func strToInt32(str string) int32 {
-	data, err := strconv.ParseInt(str, 10, 32)
-	if err != nil {
-		panic(err)
-	}
-
-	return int32(data)
-}
-
-type LazyTx struct {
-	txstring string
-}
-
-func NewLazyTx(txstring string) LazyTx {
-	return LazyTx{
-		txstring,
-	}
-}
-
-var decoder = types.NewDecoder()
-
-func (lazyTx *LazyTx) Decode() *types.StdTx {
-	tx, decodeErr := decoder(lazyTx.txstring)
-	if decodeErr != nil {
-		panic(decodeErr)
-	}
-
-	return tx
 }
