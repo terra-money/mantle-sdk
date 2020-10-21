8cd70be8c0f9bb48e7250ae13890a4342eb19def
Jesse Chung <jesse@soob.co>
2020-10-21 17:50:03 +0900

    feat(app): mantlemint

---
+++ app/mantlemint/executer.go
@@ -0,0 +1,32 @@
+package mantlemint
+
+import (
+	abcicli "github.com/tendermint/tendermint/abci/client"
+	"github.com/tendermint/tendermint/libs/log"
+	"github.com/tendermint/tendermint/mock"
+	"github.com/tendermint/tendermint/state"
+	tmdb "github.com/tendermint/tm-db"
+	TerraApp "github.com/terra-project/core/app"
+	"io/ioutil"
+	"sync"
+)
+
+func NewMantlemintExecuter(
+	db tmdb.DB,
+	AppConn abcicli.Client,
+) *state.BlockExecutor {
+	return state.NewBlockExecutor(
+		db,
+		log.NewTMLogger(ioutil.Discard),
+		AppConn,
+		mock.Mempool{},           // no mempool
+		state.MockEvidencePool{}, // no evidence pool
+	)
+}
+
+func NewMantleAppConn(
+	terraApp *TerraApp.TerraApp,
+) abcicli.Client {
+	mtx := new(sync.Mutex)
+	return abcicli.NewLocalClient(mtx, terraApp)
+}
