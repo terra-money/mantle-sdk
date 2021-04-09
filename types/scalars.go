bf37c52f147e22c7ba853b2a484a247a12a7e43c
jess <jesse@terra.money>
2021-04-09 19:32:58 +0900

    fix(types): put back JSONScalar type

--- types/scalars.go
+++ types/scalars.go
@@ -1,12 +1,30 @@
 package types
 
 import (
+	"encoding/json"
 	"github.com/graphql-go/graphql"
 )
 
 var MantleScalars = []graphql.Output{
 	Order,
 }
+
+type JSONScalar string
+type JSONScalarCustomMarshaler func(originalData interface{}, data []byte) []byte
+
+func NewJSONScalar(data interface{}, customMarshaller JSONScalarCustomMarshaler) JSONScalar {
+	bz, bzErr := json.Marshal(data)
+	if bzErr != nil {
+		panic(bzErr)
+	}
+
+	if customMarshaller != nil {
+		bz = customMarshaller(data, bz)
+	}
+
+	return JSONScalar(bz)
+}
+func (scalar *JSONScalar) IsJsonScalar() {}
 
 var Order = graphql.NewEnum(graphql.EnumConfig{
 	Name: "Order",
